bc06556e099e247964e2c280782f8aeb4483c7ea
jk872001 <jiteshkumar872001@gmail.com>
2023-01-18 10:08:27 +0530

    coding part complete

--- Lec-04/coding/tummyfull/src/App.js
+++ Lec-04/coding/tummyfull/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { restroList } from "./data"
 
 function App() {
-  return (
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        restroList.map((restro) => {
+          return <Restrorant {...restro} />
+        }
+
+        )
+      }
+
     </div>
   );
 }
 
+
+const Restrorant = ({ strCategoryThumb, strCategory }) => {
+  return (
+    <div className="restro-card">
+      <img width='200px' height='200px' src={strCategoryThumb} alt="food" />
+      <h1>{strCategory}</h1>
+    </div>
+  )
+}
 export default App;
